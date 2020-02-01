e9659780eb894d64445b106bdf85079ab14a041f
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-02-01 13:34:24 +0000

    Added time warping zones

---
+++ Assets/Scripts/TimeZone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeZone : MonoBehaviour
+{
+    public float        speedModifier = 1.0f;
+    public LayerMask    layersToAffect;
+
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (layersToAffect.HasLayer(collider.gameObject.layer))
+        {
+            TimeScaler2d ts = collider.GetComponentInParent<TimeScaler2d>();
+
+            if (ts)
+            {
+                ts.SetScale(speedModifier);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (layersToAffect.HasLayer(collider.gameObject.layer))
+        {
+            TimeScaler2d ts = collider.GetComponentInParent<TimeScaler2d>();
+
+            if (ts)
+            {
+                ts.SetScale(1.0f);
+            }
+        }
+    }
+}
